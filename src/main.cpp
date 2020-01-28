2b03521acf0a45d7405ad75f6ee5b09a27801396
psimaj <przemyslaw.simajchel@gmail.com>
2020-01-28 01:03:02 +0100

    Add main

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include "./generators/RemyBinaryTree.h"
+#include "./structures/structures.h"
+
+void parenthesesTraversal(OrderedBinaryTreeNodePtr node) {
+    if (node == nullptr) {
+        return;
+    }
+    std::cout << '(';
+    parenthesesTraversal(node->left);
+    parenthesesTraversal(node->right);
+    std::cout << ')';
+}
+
+void countingTraversal(RootedHalfGraph &graph) {
+    HalfEdgePtr startingEdge = graph.root->edge;
+    HalfEdgePtr currentEdge = startingEdge->next;
+    int visitedEdges = 1;
+    while (currentEdge != startingEdge) {
+        visitedEdges++;
+        currentEdge = currentEdge->next;
+    }
+    std::cout << visitedEdges << std::endl;
+}
+
+int main() {
+    OrderedBinaryTree tree = sampleRandomOrderedBinaryTreeRemy(5);
+    parenthesesTraversal(tree.root);
+    std::cout << std::endl;
+    RootedHalfGraph graph(tree);
+    std::cout << graph.vertices.size() << ' ' << graph.edges.size() << std::endl;
+    countingTraversal(graph);
+    return 0;
+}
