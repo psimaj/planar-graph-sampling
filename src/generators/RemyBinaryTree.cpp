0a645c1fae8538fdda84e9a0b54fc9b850c6e553
psimaj <przemyslaw.simajchel@gmail.com>
2020-01-28 01:02:50 +0100

    Add the Remy random binary tree generator

---
+++ src/generators/RemyBinaryTree.cpp
@@ -0,0 +1,60 @@
+#include <random>
+#include <chrono>
+#include <vector>
+
+#include "RemyBinaryTree.h"
+
+void reconnect(OrderedBinaryTreeNodePtr parent, OrderedBinaryTreeNodePtr old, OrderedBinaryTreeNodePtr connecting) {
+    if (parent == nullptr) {
+        return;
+    }
+    if (parent->left == old) {
+        parent->left = connecting;
+    }
+    else {
+        parent->right = connecting;
+    }
+}
+
+OrderedBinaryTree sampleRandomOrderedBinaryTreeRemy(size_t size) {
+    OrderedBinaryTree result = OrderedBinaryTree{nullptr};
+    if (size == 0) {
+        return result;
+    }
+    OrderedBinaryTreeNodePtr root(new OrderedBinaryTreeNode());
+    OrderedBinaryTreeNodePtr left(new OrderedBinaryTreeNode(root));
+    OrderedBinaryTreeNodePtr right(new OrderedBinaryTreeNode(root));
+    root->left = left;
+    root->right = right;
+    std::vector<OrderedBinaryTreeNodePtr> nodes;
+    nodes.push_back(root);
+    nodes.push_back(left);
+    nodes.push_back(right);
+
+    auto seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    std::mt19937 rnd(seed);
+    std::uniform_int_distribution<> randomBit(0, 1);
+
+    for (unsigned int i = 1; i < size; i++) {
+        std::uniform_int_distribution<> randomInt(0, i-1);
+        OrderedBinaryTreeNodePtr cut = nodes[randomInt(rnd)];
+        OrderedBinaryTreeNodePtr newNode(new OrderedBinaryTreeNode(cut->parent));
+        OrderedBinaryTreeNodePtr newLeaf(new OrderedBinaryTreeNode(newNode));
+        nodes.push_back(newNode);
+        nodes.push_back(newLeaf);
+        reconnect(cut->parent, cut, newNode);
+        newNode->left = cut;
+        newNode->right = newLeaf;
+        cut->parent = newNode;
+        if (randomBit(rnd)) {
+            std::swap(newNode->left, newNode->right);
+        }
+    }
+    for (OrderedBinaryTreeNodePtr node : nodes) {
+        if (node->isRoot()) {
+            result.root = node;
+            break;
+        }
+    }
+    return result;
+}
---
+++ src/generators/RemyBinaryTree.h
@@ -0,0 +1,8 @@
+#ifndef REMY_BINARY_TREE_H
+#define REMY_BINARY_TREE_H
+
+#include "../structures/OrderedBinaryTree.h"
+
+OrderedBinaryTree sampleRandomOrderedBinaryTreeRemy(size_t);
+
+#endif
